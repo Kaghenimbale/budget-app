480699b31c3f0ba549bce3d075662ee28c95d063
Kagheni <Kaghenimbale@gmail.com>
2023-07-18 14:04:52 +0200

    Generate transaction and grpuroup tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_17_155204) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_18_113810) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "groups_transactions", id: false, force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "transaction_id", null: false
+    t.index ["group_id", "transaction_id"], name: "index_groups_transactions_on_group_id_and_transaction_id"
+    t.index ["transaction_id", "group_id"], name: "index_groups_transactions_on_transaction_id_and_group_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.bigint "amount"
+    t.bigint "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_transactions_on_author_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -32,4 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_17_155204) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "transactions", "users", column: "author_id"
 end
