a090e11e723a5427afb051a602f442ecff351d3e
Kagheni <Kaghenimbale@gmail.com>
2023-07-19 18:37:25 +0200

    Add changes in the schemas many to many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,41 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_141152) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_183155) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "expenses", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "activities", force: :cascade do |t|
     t.string "name"
-    t.bigint "amount"
-    t.bigint "user_id", null: false
+    t.integer "amount"
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_expenses_on_user_id"
+    t.bigint "category_id", null: false
+    t.index ["author_id"], name: "index_activities_on_author_id"
+    t.index ["category_id"], name: "index_activities_on_category_id"
   end
 
-  create_table "group_expenses", id: false, force: :cascade do |t|
-    t.bigint "group_id", null: false
-    t.bigint "expense_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["expense_id", "group_id"], name: "index_group_expenses_on_expense_id_and_group_id"
-    t.index ["expense_id"], name: "index_group_expenses_on_expense_id"
-    t.index ["group_id", "expense_id"], name: "index_group_expenses_on_group_id_and_expense_id"
-    t.index ["group_id"], name: "index_group_expenses_on_group_id"
-  end
-
-  create_table "groups", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
     t.string "name"
     t.string "icon"
-    t.bigint "user_id", null: false
+    t.bigint "author_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_groups_on_user_id"
+    t.index ["author_id"], name: "index_categories_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -54,15 +76,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_141152) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "expenses", "users"
-  add_foreign_key "group_expenses", "expenses"
-  add_foreign_key "group_expenses", "groups"
-  add_foreign_key "groups", "users"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "activities", "categories"
+  add_foreign_key "activities", "users", column: "author_id"
+  add_foreign_key "categories", "users", column: "author_id"
 end
